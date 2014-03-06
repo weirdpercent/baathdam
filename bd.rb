e9e2331559ff46e3a6826e1585397b584706e760
Drew Prentice <weirdpercent@gmail.com>
2014-03-06 15:23:20 -0500

    hash-array extractions

--- bd.rb
+++ bd.rb
@@ -4,6 +4,7 @@ fo=File.open('weirdpercent.json', "r")
 mjl=MultiJson.load(fo)
 u=mjl["usage"]
 gravatar=mjl["gravatar"]
+gravurl="http://www.gravatar.com/avatar/#{gravatar}"
 cuarray=mjl["connected_users"]
 suarray=mjl["similar_users"]
 name=mjl["name"]
@@ -11,4 +12,34 @@ uname=mjl["username"]
 repoarray=mjl["repositories"]
 langarray=u["languages"]
 tz=mjl["timezone"]
-gravurl="http://www.gravatar.com/avatar/#{gravatar}"
+#cuarray, suarray & langarray are arrays of hashes that need to be broken up
+cul=cuarray.length
+sul=suarray.length
+langl=langarray.length
+cuh={}
+suh={}
+langh={}
+x=0
+y=0
+z=0
+while x < cul
+  extr=cuarray[x]
+  rearr=extr['name']
+  rearr2=extr['username']
+  cuh[rearr]=rearr2
+  x+=1
+end
+while y < sul
+  extr=suarray[y]
+  rearr=extr['name']
+  rearr2=extr['username']
+  suh[rearr]=rearr2
+  y+=1
+end
+while z < langl
+  extr=langarray[z]
+  rearr=extr['count']
+  rearr2=extr['language']
+  langh[rearr]=rearr2
+  z+=1
+end
